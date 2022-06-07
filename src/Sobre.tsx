c66000e485bccf9ebbeb554fbfbb2dc9e6af7bde
jonathanDev <joo.santos8psy@gmail.com>
2022-06-07 17:15:09 -0300

    teste

--- src/Sobre.tsx
+++ src/Sobre.tsx
@@ -10,22 +10,22 @@ function Sobre() {
         <div className='w-full flex justify-center shadow-md relative'>
             <img className='w-[98%] ' src={CheckImg} alt="" />
             <div className="w-[85%] bg-[#ebdfc9]  p-2 absolute top-4 bottom-4 flex flex-col justify-center">
-               
+
 
                 <div className="w-[88%]  flex flex-col   text-center justify-center">
 
                     <span className='text-xl font-bold'>
-                    18 de Junho de 2022 às 13:30
+                        18 de Junho de 2022 às 13:30
                     </span>
 
                     <span>
                         Será um churrasco para almoçarmos juntos.
-                        Cada pessoa pode levar suas própria bebida ou
-                        consumir no local, fica a seu critério.
+                        Peço para que levem suas bebidas. Caso queira consumir no local, 
+                        fica a seu critério.
                     </span>
                 </div>
 
-                
+
 
             </div>
 
